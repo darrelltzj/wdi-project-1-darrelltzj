9f1ab16cf4e75421dab025962c704f9a6e0b2544
Darrell Teo <darrelltzj@gmail.com>
2017-03-25 00:39:29 +0800

    Before character oop

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,18 @@
 //Create Image Generator
 //collision detection
 // mouse
+// animate images
 
 $(document).ready(function () {
   var canvasTag = $('#gameCanvas')[0]
   var ctx = canvasTag.getContext("2d")
 
-  var characterWidth = 70
-  var characterHeight = 70
+  var characterSizePercent = 0.11
+  var characterPosYOffsetPercent = 0.97
+  var characterWidth = characterSizePercent * canvasTag.width
+  var characterHeight = characterWidth
   var characterPosX = (canvasTag.width - characterWidth) / 2
-  var characterPosY = canvasTag.height - characterHeight
+  var characterPosY = characterPosYOffsetPercent * canvasTag.height - characterHeight
 
   var rightPressed = false;
   var leftPressed = false;
@@ -33,24 +36,50 @@ $(document).ready(function () {
     }
   })
 
+  // function Character() {
+  //
+  // }
+  //
+  // Character.prototype.checkMovement = function () {
+  //
+  // }
+
   function drawCharacter() {
     // ctx.beginPath();
     var character = new Image();
-    character.src = "assets/img/bettle-crawling.gif";
+    character.src = 'assets/img/cat.gif';
     ctx.drawImage(character, characterPosX, characterPosY, characterWidth, characterHeight);
     // ctx.closePath();
   }
 
-  function draw() {
-    ctx.clearRect(0, 0, canvasTag.width, canvasTag.height)
-    drawCharacter()
-    if(rightPressed && characterPosX < canvasTag.width - characterWidth) {
-      characterPosX += 7;
-    }
-    else if(leftPressed && characterPosX > 0) {
-      characterPosX -= 7;
-    }
+  function drawBackground() {
+    // ctx.beginPath();
+    var background = new Image();
+    background.src = 'assets/img/background.jpg';
+    ctx.drawImage(background, 0, 0, canvasTag.width, canvasTag.height);
+    // ctx.closePath();
   }
 
+  function resize() {
+    $('#gameCanvas')[0].width = window.innerWidth
+    $('#gameCanvas')[0].height = window.innerHeight
+    characterWidth = characterSizePercent * canvasTag.width
+    characterHeight = characterWidth
+    characterPosY = characterPosYOffsetPercent * canvasTag.height - characterHeight
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvasTag.width, canvasTag.height)
+    resize()
+    drawBackground()
+    drawCharacter()
+
+    if(rightPressed && characterPosX < canvasTag.width - characterWidth) {
+      characterPosX += 3;
+    }
+    else if(leftPressed && characterPosX > 0) {
+      characterPosX -= 3;
+    }
+  }
   setInterval(draw,10)
 })
