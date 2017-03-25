bc5bd20c51c2feb11180d2cc3a2118f2e81758f1
Darrell Teo <darrelltzj@gmail.com>
2017-03-25 15:20:03 +0800

    creating raindrops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,84 @@
 // jquery on press - OK
 // Raindrop
-//collision detection
-//Create Image Generator / movement - invert when left
+// collision detection
+// Create Image Generator / movement - invert when left
 // animate images
-//event listener for resize
-//pause
+// timer / end game
+// event listener for resize
+// pause
 // mouse
-//initialize
+// initialize
+// mouse
 
 $(document).ready(function () {
   var canvasTag = $('#gameCanvas')[0]
   var ctx = canvasTag.getContext("2d")
+
   $('#gameCanvas')[0].width = window.innerWidth
   $('#gameCanvas')[0].height = window.innerHeight
 
-  var cat = new Character(0.11, 0.97, 3)
+  var background = {
+    width: canvasTag.width,
+    height: canvasTag.height,
+    posX: 0,
+    posY: 0,
+    frames: ['background.jpg'],
+    selectedFrame: 0,
+  }
 
-  function Character(sizePercent, posYOffsetPercent, speed) {
+  var catFrames = [
+    'bunny.gif',
+    'cat.gif',
+    'bettle.gif',
+    'splat.png'
+  ]
+  var cat = new Character(0.11, 0.97, catFrames, 2, 3)
+
+  var raindropsArr = []
+  function spawnRaindrops () {
+    if (raindropsArr.length <= 7) {
+      this.raindrop = new Raindrops
+      raindropsArr.push(this.raindrop)
+    }
+  }
+  function Raindrops () {
+    this.sizePercent = 0.01
+    this.width = this.sizePercent * canvasTag.width
+    this.height = this.width
+    this.posX = this.randomX()
+    this.posY = this.height
+    this.frames = ['raindrop.png', 'splash.png', 'splat.png']
+    this.selectedFrame = 0
+    this.velocity = 0
+    this.gravity = 0.1
+  }
+  Raindrops.prototype.randomX = function () {
+    return Math.round(Math.random() * canvasTag.width)
+  }
+  Raindrops.prototype.move = function () {
+    this.posY += this.velocity
+    this.velocity += this.gravity
+  }
+
+  function Character(sizePercent, posYOffsetPercent, frames, reverseFrameIndex, velocity) {
     this.sizePercent = sizePercent
     this.posYOffsetPercent = posYOffsetPercent
+
     this.width = this.sizePercent * canvasTag.width
     this.height = this.width //need to adjust to % of canvas
     this.posX = (canvasTag.width - this.width) / 2
     this.posY = this.posYOffsetPercent * canvasTag.height - this.height
-    this.speed = speed
+    this.frames = frames
+    this.selectedFrame = 0
+    this.reverseFrameIndex = reverseFrameIndex
+
+    this.faceRight = true
+    this.velocity = velocity
+
     this.rightPressed = false;
     this.leftPressed = false;
   }
-
   Character.prototype.control = function () {
-
     $(document).on('keydown', function (e) {
       if(e.keyCode == 39) {
         this.rightPressed = true
@@ -48,14 +97,39 @@ $(document).ready(function () {
       }
     }.bind(this))
 
+    this.move()
+  }
+  Character.prototype.move = function () {
     if(this.rightPressed && this.posX < canvasTag.width - this.width) {
-      this.posX += this.speed
+      this.posX += this.velocity
+      this.faceRight = true
+      this.changeFrame()
     }
+
     else if(this.leftPressed && this.posX > 0) {
-      this.posX -= this.speed
+      this.posX -= this.velocity
+      this.faceRight = false
+      this.changeFrame()
     }
   }
-
+  Character.prototype.changeFrame = function () {
+    if (this.faceRight) {
+      if (this.selectedFrame === 1 || this.selectedFrame >= this.reverseFrameIndex) {
+        this.selectedFrame = 0
+      }
+      else {
+        this.selectedFrame += 1
+      }
+    }
+    else {
+      if (this.selectedFrame === this.frames.length - 1 || this.selectedFrame <= this.reverseFrameIndex - 1) {
+        this.selectedFrame = 2
+      }
+      else {
+        this.selectedFrame += 1
+      }
+    }
+  }
   Character.prototype.resize = function () {
     this.width = this.sizePercent * canvasTag.width
     this.height = this.width //need to adjust to % of canvas
@@ -63,20 +137,10 @@ $(document).ready(function () {
     this.posY = this.posYOffsetPercent * canvasTag.height - this.height
   }
 
-  function drawCat() {
-    // ctx.beginPath();
-    var catImage = new Image();
-    catImage.src = 'assets/img/bunny.gif';
-    ctx.drawImage(catImage, cat.posX, cat.posY, cat.width, cat.height);
-    // ctx.closePath();
-  }
-
-  function drawBackground() {
-    // ctx.beginPath();
-    var background = new Image();
-    background.src = 'assets/img/background.jpg';
-    ctx.drawImage(background, 0, 0, canvasTag.width, canvasTag.height);
-    // ctx.closePath();
+  function createFrame(item) {
+    this.image = new Image()
+    this.image.src = 'assets/img/' + item.frames[item.selectedFrame]
+    ctx.drawImage(this.image, item.posX, item.posY, item.width, item.height)
   }
 
   function resize() {
@@ -88,10 +152,20 @@ $(document).ready(function () {
   function run() {
     ctx.clearRect(0, 0, canvasTag.width, canvasTag.height)
     // resize() // use listener
-    drawBackground()
-    drawCat()
+    createFrame(background)
+    spawnRaindrops()
+    raindropsArr.filter(function (raindrop) {
+      // console.log(raindrop.posY <= canvasTag.height,raindropsArr)
+      return raindrop.posY <= canvasTag.height
+    })
+    // console.log(raindropsArr.length)
+    raindropsArr.forEach(function (raindrop) {
+      createFrame(raindrop)
+      raindrop.move()
+    })
+    // console.log(raindropsArr)
+    createFrame(cat)
     cat.control()
-
   }
   setInterval(run,10)
 })
